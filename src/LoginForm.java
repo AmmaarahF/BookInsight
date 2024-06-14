277274b833dac751436361b9786fefc78e238d91
ammaa <ammaarahfakier01@gmail.com>
2024-06-14 14:06:26 +0200

    linking to login

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -1,7 +1,29 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class LoginForm {
+public class LoginForm extends JFrame {
     private JTextField textField2;
     private JTextField textField1;
     private JButton LOGINButton;
+    private JPanel loginPanel;
+
+    public LoginForm() {
+        setTitle("Login");
+        setContentPane(loginPanel);
+        setMinimumSize(new Dimension(800, 500));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            LoginForm loginForm = new LoginForm();
+            loginForm.setVisible(true);
+            loginForm.setLocationRelativeTo(null);
+        });
+    }
 }
+
+
+
+
+
--- src/RegistrationForm.java
+++ src/RegistrationForm.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class RegistrationForm extends JDialog{
+public class RegistrationForm extends JDialog {
     private JTextField regName;
     private JTextField regEmail;
     private JPasswordField regPassword;
@@ -11,37 +11,49 @@ public class RegistrationForm extends JDialog{
     private JButton loginButton;
     private JPanel registerPanel;
 
-    public RegistrationForm(JFrame parent){
+    public RegistrationForm(JFrame parent) {
         super(parent);
         setTitle("Register");
         setContentPane(registerPanel);
         setMinimumSize(new Dimension(850, 550));
         setModal(true);
-        setLocationRelativeTo(parent);
+        setLocationRelativeTo(null);
 
         signUpButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                registerUser();
+
             }
         });
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                LoginForm loginForm = new LoginForm();
+                loginForm.setVisible(true);
+                loginForm.pack();
+                loginForm.setLocationRelativeTo(null);
+                dispose();
+            }
+        });
+
         setVisible(true);
     }
+
     private void registerUser() {
         String name = regName.getText();
         String email = regEmail.getText();
         String password = String.valueOf(regPassword.getPassword());
 
-        if (name.isEmpty() || email.isEmpty() || password.isEmpty()){
-            JOptionPane.showMessageDialog(this, "Please enter all fields" , "Try Again", JOptionPane.ERROR_MESSAGE);
+        if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Please enter all fields", "Try Again", JOptionPane.ERROR_MESSAGE);
             return;
         }
 
 
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         RegistrationForm regForm = new RegistrationForm(null);
     }
-
 }
